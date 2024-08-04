ff8a3d29754a23f5e3b6bd3079b78f8a063a7a22
farxodnazarov <farhodnazarov002gmail.com>
2024-08-04 16:23:56 +0500

    changed

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: null,
+  orders: [],
   TotalPrice: 0,
   TotalProduct: 0,
 };
@@ -21,11 +22,16 @@ export const productSlice = createSlice({
       } else {
         item.amount += 1;
       }
+      productSlice.caseReducers.calculateOrder(state);
     },
     decrimentProduct: (state, { payload }) => {
       const item = state.products.find((product) => product.id == payload);
 
       item.amount -= 1;
+      productSlice.caseReducers.calculateOrder(state);
+    },
+    calculateOrder: (state) => {
+      state.orders = state.products.filter((prod) => prod.amount);
     },
   },
 });
